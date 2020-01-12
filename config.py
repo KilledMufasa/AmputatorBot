d28159442bcf40ccd9c2abb1eff95282c6dc21c0
Killed_Mufasa <jsvanderburgh@gmail.com>
2020-01-12 21:07:35 +0100

    Updated template config.py with more info and new example user agents

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ username = "RedditUsername"
 password = "RedditPassword"
 client_id = "RedditClientId"
 client_secret = "RedditClientSecret"
-headers = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0',
-           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36']
+headers = ['Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3202.84 Mobile Safari/537.36',
+           'Mozilla/5.0 (Linux; Android 9; CLT-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3945.116 Mobile Safari/537.36']
           # The above user-agents are just examples, change them before you run the scripts!
           # It's highly recommended to use user agents that are new, common and not blacklisted.
-          # The more user-agents, the better. These meassures are there to prevent 403-errors.
+          # Also, to prevent weird redirects, consider using only mobile chrome user agents.
+          # The more user-agents, the better. These meassures are here to prevent 403-errors.
