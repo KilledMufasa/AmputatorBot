b3346a996e93ae1c6816f236576b55ddee64f5f0
Joris van der Burgh <jsvanderburgh2@gmail.com>
2020-08-23 15:28:21 +0200

    Changed tweet template, removed redundant variables

--- helpers/twitter_utils.py
+++ helpers/twitter_utils.py
@@ -40,8 +40,8 @@ def get_twitterer_name(tweet):
 # Generate a reply tweet
 def generate_tweet(links):
     # Initialize all variables
-    alt_link, canonical_text_latest, canonical_text_list, canonical_text = "", "", "", ""
-    n_amp_urls, n_canonicals = 0, 0
+    canonical_text_latest, canonical_text_list, canonical_text = "", "", ""
+    n_canonicals = 0
 
     # Loop through all links, and generate the canonical link part of the comment
     for link in links:
@@ -53,14 +53,14 @@ def generate_tweet(links):
 
     if n_canonicals >= 1:
         if n_canonicals == 1:
-            intro_who_wat = "It looks like you tweeted a cached AMP link. "
+            intro_who_wat = "It looks like you shared a cached AMP link. "
             canonical_text = canonical_text_latest
-            intro_maybe = "You might want to visit the non-cached page instead: "
+            intro_maybe = "You might want to visit the uncached page instead: "
 
         else:
-            intro_who_wat = "It looks like you tweeted some cached AMP links. "
+            intro_who_wat = "It looks like you shared some cached AMP links. "
             canonical_text = canonical_text_list
-            intro_maybe = "You might want to visit the non-cached pages instead: "
+            intro_maybe = "You might want to visit the uncached pages instead: "
 
         tweet_text = intro_who_wat + intro_maybe + canonical_text
 
